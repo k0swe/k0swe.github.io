e92492068f6e65bd6a6cbb64fbfe778df49d747d
Chris Keller <xylo04@gmail.com>
2021-08-06 17:23:53 -0600

    Checkout _site in action instead of script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 COMMIT=`git rev-parse --short HEAD`
-rm -rf _site
-git clone --depth 1 git@github.com:k0swe/k0swe.github.io.git -b public _site
 bundle exec jekyll build
 cd _site
 touch .nojekyll
