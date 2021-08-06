4030b1c5e4e3cc1bcbcb67462eeb9e7fda5a242f
Chris Keller <xylo04@gmail.com>
2021-08-06 17:29:20 -0600

    inline deploy script in action

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-COMMIT=`git rev-parse --short HEAD`
-bundle install
-bundle exec jekyll build
-cd _site
-touch .nojekyll
-git add -A
-git commit -m "Generated site at $COMMIT"
-git push origin public
