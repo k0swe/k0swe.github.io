86664cc3c9e78db4584e5c645636a374f2a32086
Chris Keller <xylo04@gmail.com>
2022-02-08 10:07:14 -0700

    Make image resizer smarter

--- resize-images.sh
+++ resize-images.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-find assets/ -iname "*.jpg" -exec convert {} -resize 1024x1024\> -strip {} \;
-find assets/ -iname "*.png" -exec convert {} -resize 1024x1024\> -strip {} \;
-find assets/ -iname "*.gif" -exec convert {} -resize 1024x1024\> -strip {} \;
+max_dimension=1024
+images=$(find assets/ -iname "*.gif" -o -iname "*.png" -o -iname "*.jpg" | sort)
+for i in $images; do
+  width=$(convert $i -print "%w" /dev/null)
+  height=$(convert $i -print "%h" /dev/null)
+  if [ "$width" -gt "$max_dimension" -o "$height" -gt "$max_dimension" ]; then
+    echo "Shrinking $i"
+    convert $i -resize "$max_dimension"x"$max_dimension"\> -strip $i
+  fi
+done
