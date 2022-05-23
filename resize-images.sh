1f4119fe51b6cff058d28cfa62841eb3ecc91ae2
Chris Keller <xylo04@gmail.com>
2022-05-23 05:09:01 +0000

    Add LARCfest post
    Also add webp to the resize-images script.

--- resize-images.sh
+++ resize-images.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 max_dimension=1024
-images=$(find assets/ -iname "*.gif" -o -iname "*.png" -o -iname "*.jpg" | sort)
+images=$(find assets/ -iname "*.gif" -o -iname "*.png" -o -iname "*.jpg" -o -iname "*.webp" | sort)
 for i in $images; do
   width=$(convert $i -print "%w" /dev/null)
   height=$(convert $i -print "%h" /dev/null)
